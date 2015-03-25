4629470634d38abc9a1cfe735744aca43a703bb7
Alex Bath <alex@oakdalesoft.com>
2015-03-25 08:22:44 +0000

    Clean-up

---
+++ src/main/resources/import.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO Book (id, title, price, description, nbofpage, illustrations) VALUES (0, 'Functional Programming in Scala', 49.99, 'Functional Programming in Scala is a serious tutorial for programmers looking to learn FP and apply it to the everyday business of coding.', 301, false);
+
+INSERT INTO Book (id, title, price, description, nbofpage, illustrations) VALUES (1, 'Big Data Analytics Beyond Hadoop: Real-Time Applications with Storm, Spark, and More Hadoop Alternatives', 55.99, 'Master alternative Big Data technologies that can do what Hadoop can''t: real-time analytics and iterative machine learning', 240, true);
+
+
